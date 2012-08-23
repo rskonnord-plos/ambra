8ef56042d42dfa2142655a7bde80f0e03c069bc6
Alex Kudlick <akudlick@plos.org>
2012-08-23 21:03:31 +0000

    merge from solr upgrade branch

--- webapp/src/main/java/org/ambraproject/search/service/SolrServerFactory.java
+++ webapp/src/main/java/org/ambraproject/search/service/SolrServerFactory.java
@@ -1,18 +1,10 @@
 /*
  * $HeadURL$
  * $Id$
- *
- * Copyright (c) 2006-2010 by Public Library of Science
- * http://plos.org
- * http://ambraproject.org
- *
+ * Copyright (c) 2006-2012 by Public Library of Science http://plos.org http://ambraproject.org
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -23,7 +15,7 @@ package org.ambraproject.search.service;
 
 import org.apache.commons.configuration.Configuration;
 import org.apache.solr.client.solrj.SolrServer;
-import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,9 +39,9 @@ public class SolrServerFactory {
     String serverUrl = configuration.getString(URL_CONFIG_PARAM, null);
     if (serverUrl != null) {
       log.info("Creating SolrServer instance at " + serverUrl);
-      server = new CommonsHttpSolrServer(serverUrl);
-      ((CommonsHttpSolrServer) server).setConnectionManagerTimeout(60000l);
-      ((CommonsHttpSolrServer) server).setSoTimeout(60000);
+      server = new HttpSolrServer(serverUrl);
+      ((HttpSolrServer) server).setConnectionTimeout(60000);
+      ((HttpSolrServer) server).setSoTimeout(60000);
     } else {
       log.warn(URL_CONFIG_PARAM + " not set. SolrServer instance will not be created.");
     }
--- webapp/src/test/java/org/ambraproject/testutils/EmbeddedSolrServerFactory.java
+++ webapp/src/test/java/org/ambraproject/testutils/EmbeddedSolrServerFactory.java
@@ -1,18 +1,10 @@
 /*
  * $HeadURL$
  * $Id$
- *
- * Copyright (c) 2006-2011 by Public Library of Science
- * http://plos.org
- * http://ambraproject.org
- *
+ * Copyright (c) 2006-2012 by Public Library of Science http://plos.org http://ambraproject.org
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -83,7 +75,7 @@ public class EmbeddedSolrServerFactory extends SolrServerFactory {
    * @throws Exception - URISyntaxException when loading resources or else Exception when setting up the server
    */
   public EmbeddedSolrServerFactory() throws Exception {
-    this("solr/test-solr-schema.xml", "solr/test-solr-config.xml");
+    this("solr/conf/test-solr-schema.xml", "solr/conf/test-solr-config.xml");
   }
 
   public void tearDown() throws Exception {
@@ -224,8 +216,16 @@ public class EmbeddedSolrServerFactory extends SolrServerFactory {
 
     @Override
     public void tearDown() throws Exception {
-      super.preTearDown();
-      super.tearDown();
+
+      try {
+
+        super.preTearDown();
+        super.tearDown();
+
+      } catch (Exception e){
+        log.warn("Embedded Solr server tear down Failed.");
+      }
+
     }
   }
 
