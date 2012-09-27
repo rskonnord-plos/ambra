36ce7bf640cf2c2b496408c7c3f2fadc23e7f9f7
Joe Osowski <josowski@plos.org>
2012-09-27 16:44:05 +0000

    Removing deprecated import

--- base/src/main/java/org/ambraproject/service/article/FetchArticleServiceImpl.java
+++ base/src/main/java/org/ambraproject/service/article/FetchArticleServiceImpl.java
@@ -58,7 +58,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
