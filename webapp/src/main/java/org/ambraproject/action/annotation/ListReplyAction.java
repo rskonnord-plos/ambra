dbdafa8f23534fabbe1064929e00bbb9b4f38a65
Ryan Skonnord <rskonnord@plos.org>
2012-10-22 23:33:28 +0000

    FEND-301 Make author info visible to comment thread view

--- webapp/src/main/java/org/ambraproject/action/annotation/ListReplyAction.java
+++ webapp/src/main/java/org/ambraproject/action/annotation/ListReplyAction.java
@@ -22,11 +22,16 @@ package org.ambraproject.action.annotation;
 import org.ambraproject.action.BaseActionSupport;
 import org.ambraproject.service.annotation.AnnotationService;
 import org.ambraproject.service.article.ArticleService;
+import org.ambraproject.service.article.FetchArticleService;
 import org.ambraproject.views.AnnotationView;
+import org.ambraproject.views.AuthorExtra;
 import org.ambraproject.views.article.ArticleInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
+import org.w3c.dom.Document;
+
+import java.util.List;
 
 /**
  * Action class to get a list of replies to annotations.
@@ -35,17 +40,23 @@ import org.springframework.beans.factory.annotation.Required;
 public class ListReplyAction extends BaseActionSupport {
   private static final Logger log = LoggerFactory.getLogger(ListReplyAction.class);
 
+  protected AnnotationService annotationService;
+  private ArticleService articleService;
+  private FetchArticleService fetchArticleService;
+
   private Long root;
   private AnnotationView baseAnnotation;
   private ArticleInfo articleInfo;
-  protected AnnotationService annotationService;
-  private ArticleService articleService;
+  private List<AuthorExtra> authorExtras;
 
   @Override
   public String execute() throws Exception {
     try {
       baseAnnotation = annotationService.getFullAnnotationView(root);
       articleInfo = articleService.getBasicArticleView(baseAnnotation.getArticleID());
+
+      Document doc = fetchArticleService.getArticleDocument(articleInfo);
+      authorExtras = fetchArticleService.getAuthorAffiliations(doc);
     } catch (Exception ae) {
       log.error("Could not list all replies for root: " + root, ae);
       addActionError("Reply fetching failed with error message: " + ae.getMessage());
@@ -58,6 +69,7 @@ public class ListReplyAction extends BaseActionSupport {
   public void setRoot(final Long root) {
     this.root = root;
   }
+
   /**
    * @return Returns the baseAnnotation.
    */
@@ -69,6 +81,10 @@ public class ListReplyAction extends BaseActionSupport {
     return articleInfo;
   }
 
+  public List<AuthorExtra> getAuthorExtras() {
+    return authorExtras;
+  }
+
   @Required
   public void setArticleService(ArticleService articleService) {
     this.articleService = articleService;
@@ -78,4 +94,10 @@ public class ListReplyAction extends BaseActionSupport {
   public void setAnnotationService(final AnnotationService annotationService) {
     this.annotationService = annotationService;
   }
+
+  @Required
+  public void setFetchArticleService(FetchArticleService fetchArticleService) {
+    this.fetchArticleService = fetchArticleService;
+  }
+
 }
