94a0cc3dce74366336d5429380867e83fb19ecdf
Ryan Skonnord <rskonnord@plos.org>
2012-10-29 21:16:15 +0000

    Revert accidental removal of third-party attribution and licensing

--- webapp/src/main/webapp/javascript/jquery.uniform.js
+++ webapp/src/main/webapp/javascript/jquery.uniform.js
@@ -1,25 +1,24 @@
 /*
- *
- *  * $HeadURL$
- *  * $Id$
- *  *
- *  * Copyright (c) 2006-2012 by Public Library of Science
- *  * http://plos.org
- *  * http://ambraproject.org
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  *     http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
- *
- */
+
+Uniform v1.7.5
+Copyright © 2009 Josh Pyles / Pixelmatrix Design LLC
+http://pixelmatrixdesign.com
+
+Requires jQuery 1.4 or newer
+
+Much thanks to Thomas Reynolds and Buck Wilson for their help and advice on this
+
+Disabling text selection is made possible by Mathias Bynens <http://mathiasbynens.be/>
+and his noSelect plugin. <http://github.com/mathiasbynens/noSelect-jQuery-Plugin>
+
+Also, thanks to David Kaneda and Eugene Bond for their contributions to the plugin
+
+License:
+MIT License - http://www.opensource.org/licenses/mit-license.php
+
+Enjoy!
+
+*/
 
 (function($) {
   $.uniform = {
