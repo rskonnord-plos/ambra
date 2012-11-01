824bb83b68e8b30da788cd9285ff8388b173315a
Joe Osowski <josowski@plos.org>
2012-11-01 20:08:46 +0000

    A little cleanup on the migration sql

--- base/src/main/resources/org/ambraproject/service/migration/migrate_ambra_2_4_7.sql
+++ base/src/main/resources/org/ambraproject/service/migration/migrate_ambra_2_4_7.sql
@@ -1,3 +1,2 @@
-use ambra;
-ALTER TABLE article ADD COLUMN strkImgURI VARCHAR(50);
-ALTER TABLE annotation ADD COLUMN highlightedText TEXT;
+ALTER TABLE article ADD COLUMN strkImgURI VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_bin null after url;
+ALTER TABLE annotation ADD COLUMN highlightedText TEXT CHARACTER SET utf8 COLLATE utf8_bin null after competingInterestBody;
