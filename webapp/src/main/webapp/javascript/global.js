1c784199d43923f900cd3eb969c6567a3612721b
Joe Osowski <josowski@plos.org>
2012-11-02 16:06:30 +0000

    Adding in javascript functions FEND-332

--- webapp/src/main/webapp/javascript/global.js
+++ webapp/src/main/webapp/javascript/global.js
@@ -925,3 +925,49 @@ Number.prototype.format = function(c, d, t){
   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d +
     Math.abs(n - i).toFixed(c).slice(2) : "");
 };
+
+//******************************
+//Browse / issue page functions
+//******************************
+// on window load
+$(window).load(function(){
+  $('.journal_issues').doOnce(function(){
+    this.journalArchive({
+      navID : '#journal_years',
+      slidesContainer : '#journal_slides',
+      initialTab : 0
+    });
+  });
+});
+
+(function($){
+  $.fn.journalArchive = function(options) {
+    defaults = {
+      navID : '',
+      slidesContainer : '',
+      initialTab : 0,
+    };
+    var options = $.extend(defaults, options);
+    var $navContainer = $(options.navID);
+    var $slidesContainer = $(options.slidesContainer);
+    init = function(){
+      $navContainer.find('li').eq(options.initialTab).addClass('selected');
+      var initial_slide = $slidesContainer.find('li.slide').eq(options.initialTab);
+      var aheight = initial_slide.height();
+      $slidesContainer.css('height', aheight);
+      initial_slide.addClass('selected').fadeIn();
+    };
+    $navContainer.find('li a').on('click', function(e){
+      e.preventDefault();
+      $this = $(this);
+      var target = $this.attr('href');
+      $navContainer.find('li.selected').removeClass('selected');
+      $slidesContainer.find('li.slide.selected').removeClass('selected').fadeOut();
+      $this.parent('li').addClass('selected');
+      var targetElement = $slidesContainer.find('li' +target);
+      targetElement.addClass('selected').fadeIn();
+      $slidesContainer.animate({'height': targetElement.height()});
+    });
+    init();
+  };
+})(jQuery);
