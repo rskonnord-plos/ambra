709db4cf89d3420710eebe82b120145a652a0ae0
Joe Osowski <josowski@plos.org>
2012-11-08 17:05:41 +0000

    Fixing schema

--- base/src/main/resources/org/ambraproject/service/migration/migrate_ambra_2_4_0_part1.sql
+++ base/src/main/resources/org/ambraproject/service/migration/migrate_ambra_2_4_0_part1.sql
@@ -1,10 +1,10 @@
 create table pingback (
     pingbackID bigint not null auto_increment,
+    articleID bigint not null CHARACTER SET utf8 COLLATE utf8_bin null;,
+    url varchar(255) not null CHARACTER SET utf8 COLLATE utf8_bin null;,
+    title varchar(255),
     lastModified datetime not null,
     created datetime not null,
-    articleID bigint not null,
-    url varchar(255) not null,
-    title varchar(255),
     primary key (pingbackID),
     unique (articleID, url)
 );
