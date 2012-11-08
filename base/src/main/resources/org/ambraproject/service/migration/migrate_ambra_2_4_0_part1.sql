5d4fea4ca133d94b9ce4aa2f713ed32a304e2520
Joe Osowski <josowski@plos.org>
2012-11-08 17:20:10 +0000

    Must ... drink ... caffeine ... before ... commit

--- base/src/main/resources/org/ambraproject/service/migration/migrate_ambra_2_4_0_part1.sql
+++ base/src/main/resources/org/ambraproject/service/migration/migrate_ambra_2_4_0_part1.sql
@@ -1,8 +1,8 @@
 create table pingback (
     pingbackID bigint not null auto_increment,
-    articleID bigint not null CHARACTER SET utf8 COLLATE utf8_bin null;,
-    url varchar(255) not null CHARACTER SET utf8 COLLATE utf8_bin null;,
-    title varchar(255),
+    articleID bigint not null,
+    url varchar(255) CHARACTER SET utf8 COLLATE utf8_bin not null,
+    title varchar(255) CHARACTER SET utf8 COLLATE utf8_bin null,
     lastModified datetime not null,
     created datetime not null,
     primary key (pingbackID),
