5911d2ce2a1f8ba6b89aa7cb14ac0dc29c651884
Ryan Skonnord <rskonnord@plos.org>
2012-11-14 19:33:58 +0000

    FEND-513 Enclose authors list in div to fix text wrapping

--- webapp/src/main/webapp/javascript/related_content.js
+++ webapp/src/main/webapp/javascript/related_content.js
@@ -48,9 +48,10 @@ $(function () {
             var views = $('<div></div>').attr("class", "views").html("<span>" + docs[i].counter_total_all + "</span> Views");
             var citations = $('<div></div>').attr("class", "citations").html("<span>" + docs[i].alm_scopusCiteCount + "</span> Citations");
             var title = $('<h4></h4>').append('<a href="http://dx.plos.org/' + docs[i].id + '">' + docs[i].title_display + "</a>");
+            var authors = $('<div></div>').append(docs[i].author_display.join(", "));
             var meta = $('<div></div>').attr("class", "meta").append(docs[i].article_type + " | " +
               docs[i].publication_date + " | " + docs[i].journal + "<br> doi:" + docs[i].id);
-            var info = $('<div></div>').attr('class', 'info').append(title, docs[i].author_display.join(", "), meta);
+            var info = $('<div></div>').attr('class', 'info').append(title, authors, meta);
             var li = $('<li></li>').attr("class", "cf").append(views, citations, info);
             $('div[id="more_by_authors"] > ul').append(li);
           }
