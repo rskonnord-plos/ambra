987e54530ff7b286953b676f343c0ff98eb93052
Jennifer Song <jsong@plos.org>
2012-11-30 23:16:53 +0000

    FEND-677 fix name collision

--- webapp/src/main/webapp/javascript/searchResults.js
+++ webapp/src/main/webapp/javascript/searchResults.js
@@ -28,11 +28,11 @@ $(document).ready(
         $(element).removeAttr('checked');
       });
 
-      $("#searchForm").submit();
+      $("#searchStripForm").submit();
     });
 
     $("input[name|='filterJournals']").click(function(eventObj) {
-      $("#searchForm").submit();
+      $("#searchStripForm").submit();
     });
 
     $("#clearSubjectFilter").click(function(eventObj) {
@@ -40,11 +40,11 @@ $(document).ready(
         $(element).removeAttr('checked');
       });
 
-      $("#searchForm").submit();
+      $("#searchStripForm").submit();
     });
 
     $("input[name|='filterSubjects']").click(function(eventObj) {
-      $("#searchForm").submit();
+      $("#searchStripForm").submit();
     });
 
     $("#clearAuthorFilter").click(function(eventObj) {
@@ -52,15 +52,15 @@ $(document).ready(
         $(element).removeAttr('checked');
       });
 
-      $("#searchForm").submit();
+      $("#searchStripForm").submit();
     });
 
     $("input[name|='filterAuthors']").click(function(eventObj) {
-      $("#searchForm").submit();
+      $("#searchStripForm").submit();
     });
 
     $("#sortPicklist").change(function(eventObj) {
-      $("#searchForm").submit();
+      $("#searchStripForm").submit();
     });
 
     //***************************************
@@ -509,7 +509,7 @@ $(document).ready(
       $.ajax({
         type: 'POST',
         url: '/search/saveSearch.action',
-        data: $('#searchForm').serialize(),
+        data: $('#searchStripForm').serialize(),
         dataFilter: function (data, type) {
           return data.replace(/(\/\*|\*\/)/g, '');
         },
