b609ff245e8c7a20f342bdb79c493fc52d9cee04
Joe Osowski <josowski@plos.org>
2013-02-05 17:55:00 -0800

    Removing deprecated reference SE-168

--- base/src/main/java/org/ambraproject/service/raptor/RaptorServiceImpl.java
+++ base/src/main/java/org/ambraproject/service/raptor/RaptorServiceImpl.java
@@ -31,7 +31,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.List;
 
