b144ad3eb83585e99017e16e1780b0a4d9b9be58
John Callaway <jcallaway@plos.org>
2013-03-26 10:09:06 -0700

    Merge in Joe's changes to these classes (which I am moving into ambra-queue).

--- queue/src/main/java/org/ambraproject/queue/CamelSender.java
+++ queue/src/main/java/org/ambraproject/queue/CamelSender.java
@@ -1,10 +1,18 @@
 /*
  * $HeadURL$
  * $Id$
- * Copyright (c) 2006-2013 by Public Library of Science http://plos.org http://ambraproject.org
+ *
+ * Copyright (c) 2006-2010 by Public Library of Science
+ * http://plos.org
+ * http://ambraproject.org
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0Unless required by applicable law or agreed to in writing, software
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -16,29 +24,43 @@ package org.ambraproject.queue;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Required;
 import org.w3c.dom.Document;
+import java.util.Map;
 
 /**
  * Apache Camel sender.
  * @author Dragisa Krsmanovic
+ * @author Joe Osowski
  */
 public class CamelSender implements MessageSender {
 
   private ProducerTemplate producerTemplate;
 
+  /**
+   * @inheritDoc
+   */
   @Required
   public void setProducerTemplate(ProducerTemplate producerTemplate) {
     this.producerTemplate = producerTemplate;
   }
 
+  /**
+   * @inheritDoc
+   */
   public void sendMessage(String destination, String body) {
-
-    System.err.println(String.format("CALLAWAY: CamelSender.sendMessage sending %d bytes to %s",
-        body.length(), destination));
-
     producerTemplate.sendBody(destination, body);
   }
 
+  /**
+   * @inheritDoc
+   */
   public void sendMessage(String destination, Document body) {
     producerTemplate.sendBody(destination, body);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public void sendMessage(String destination, Object body, Map<String,Object> headers) {
+    producerTemplate.sendBodyAndHeaders(destination, body, headers);
   }
 }
--- queue/src/main/java/org/ambraproject/queue/MessageSender.java
+++ queue/src/main/java/org/ambraproject/queue/MessageSender.java
@@ -1,10 +1,18 @@
 /*
  * $HeadURL$
  * $Id$
- * Copyright (c) 2006-2013 by Public Library of Science http://plos.org http://ambraproject.org
+ *
+ * Copyright (c) 2006-2010 by Public Library of Science
+ * http://plos.org
+ * http://ambraproject.org
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0Unless required by applicable law or agreed to in writing, software
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -15,9 +23,12 @@ package org.ambraproject.queue;
 
 import org.w3c.dom.Document;
 
+import java.util.Map;
+
 /**
  * Message sender interface. 
  * @author Dragisa Krsmanovic
+ * @author Joe Osowski
  */
 public interface MessageSender {
 
@@ -34,4 +45,12 @@ public interface MessageSender {
    * @param body Message body,
    */
   void sendMessage(String destination, Document body);
+
+  /**
+   * Send a generic message with headers
+   * @param destination the destination endpoint
+   * @param body the body of the queue message
+   * @param headers headers to append to the queue message
+   */
+  public void sendMessage(String destination, Object body, Map<String,Object> headers);
 }
