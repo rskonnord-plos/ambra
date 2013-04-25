21ec69f688537d67d001ad5bc2f766aee333d15f
Joe Osowski <josowski@plos.org>
2013-04-25 16:14:13 -0700

    Moving function to global so it can be reused

--- webapp/src/main/webapp/javascript/global.js
+++ webapp/src/main/webapp/javascript/global.js
@@ -1,6 +1,4 @@
 /*
- * $HeadURL$
- * $Id$
  * Copyright (c) 2006-2012 by Public Library of Science http://plos.org http://ambraproject.org
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1587,3 +1585,50 @@ $(function() {
   }
 });
 
+/*
+ * jQuery UI Autocomplete HTML Extension
+ *
+ * Copyright 2010, Scott González (http://scottgonzalez.com)
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * http://github.com/scottgonzalez/jquery-ui-extensions
+ */
+
+// HTML extension to autocomplete borrowed from
+// https://github.com/scottgonzalez/jquery-ui-extensions/blob/master/autocomplete/jquery.ui.autocomplete.html.js
+
+(function( $ ) {
+
+  var proto = $.ui.autocomplete.prototype,
+    initSource = proto._initSource;
+
+  function filter( array, term ) {
+    var matcher = new RegExp( $.ui.autocomplete.escapeRegex(term), "i" );
+    return $.grep( array, function(value) {
+      return matcher.test( $( "<div>" ).html( value.label || value.value || value ).text() );
+    });
+  }
+
+  $.extend( proto, {
+    _initSource: function() {
+      if ($.isArray(this.options.source) ) {
+        this.source = function( request, response ) {
+          response( filter( this.options.source, request.term ) );
+        };
+      } else {
+        initSource.call( this );
+      }
+    },
+
+    _renderItem: function( ul, item) {
+      return $( "<li></li>" )
+        .data( "item.autocomplete", item )
+        .append( $( "<a style=\"line-height: "
+          + (item.value ? 0.9 : 2)
+          + "; font-size: 12px;\"></a>" )
+          [item.type == "html" ? "html" : "text"]( item.label ) )
+        .appendTo( ul );
+    }
+  });
+
+})( jQuery );
