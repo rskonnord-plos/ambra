6f3b0d8e4b42e35f0f37997c3fcf060fae5a984b
Jen Song <jsong@plos.org>
2013-07-03 15:58:36 -0700

    FEND-1143 most simple solution to the problem

--- base/src/main/java/org/ambraproject/service/search/SolrSearchService.java
+++ base/src/main/java/org/ambraproject/service/search/SolrSearchService.java
@@ -44,6 +44,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
+import java.util.TimeZone;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.regex.Pattern;
@@ -622,6 +623,7 @@ public class SolrSearchService implements SearchService {
     StringBuilder fq = new StringBuilder();
 
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
     String sDate = sdf.format(startDate) + "T00:00:00Z";
     String eDate = sdf.format(endDate) + "T00:00:00Z";
 
