5a0ba0147ac8d5b02290633599e33b6444d0e2f3
Mamta Singh <msingh@plos.org>
2013-08-06 16:14:30 -0700

    FEND-1208. creating new schema for news article

--- base/src/main/resources/org/ambraproject/service/migration/migrate_ambra_2_7_4_part1.sql
+++ base/src/main/resources/org/ambraproject/service/migration/migrate_ambra_2_7_4_part1.sql
@@ -1,6 +1,18 @@
 create table news (
-  sortOrder integer not null default 0,
-  articleID bigint not null default 0,
-  primary key (sortOrder),
-  constraint foreign key (articleID) references article(articleID),
+  newsID bigint not null auto_increment,
+  displayName varchar(255) CHARACTER SET utf8 COLLATE utf8_bin default null,
+  journalID bigint not null,
+  created datetime null,
+  lastModified datetime null,
+  PRIMARY KEY (newsID, journalID),
+  constraint foreign key (journalID) references journal (journalID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table newsArticleList (
+  newsID bigint not null,
+  articleID bigint not null,
+  sortOrder int(11) not null,
+  PRIMARY KEY (newsID, articleID, sortOrder),
+  constraint foreign key (newsID) references news (newsID),
+  constraint foreign key (articleID) references article (articleID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
