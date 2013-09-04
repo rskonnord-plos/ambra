a4d5f473510048d80160c4a7017df1cd39726308
Joe Osowski <joe.osowski@gmail.com>
2013-09-04 14:18:49 -0700

    Removing deprecated code

--- webapp/src/main/webapp/javascript/global.js
+++ webapp/src/main/webapp/javascript/global.js
@@ -200,9 +200,8 @@ function onReadyMainContainer() {
 // and then everytime when the tab content loads via Pjax.
 
 function initMainContainer() {
-  var $nav_article = $('#nav-article');
-
   var $figure_thmbs = $('#figure-thmbs');
+
   if ($figure_thmbs.length) {
     $lnks = $figure_thmbs.find('.item a');
     $wrap = $figure_thmbs.find('div.wrapper');
