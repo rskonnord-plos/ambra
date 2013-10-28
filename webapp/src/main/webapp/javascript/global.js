66947644605f8797ee4fb6bbe7cccea3caffc723
Mamta Singh <msingh@plos.org>
2013-10-28 14:36:54 -0700

    AMEC-1853: Figure drawer display

--- webapp/src/main/webapp/javascript/global.js
+++ webapp/src/main/webapp/javascript/global.js
@@ -240,6 +240,9 @@ function initMainContainer() {
   $figure_thmbs.insertBefore($('.article .articleinfo'));
 
   if ($figure_thmbs.length) {
+    $figure_thmbs.css('visibility', 'visible');
+    $('<h3>Figures</h3>').insertBefore($figure_thmbs);
+
     $lnks = $figure_thmbs.find('.item a');
     $wrap = $figure_thmbs.find('div.wrapper');
     if ($lnks.length) {
@@ -249,21 +252,6 @@ function initMainContainer() {
         ref = $(this).data('uri');
         launchModal(doi, ref, 'fig');
       });
-      $fig_tog = $('<span>Hide Figures</span>').toggle(function () {
-          $wrap.hide();
-          $figure_thmbs.find('div.buttons').hide();
-          $figure_thmbs.find('div.controls').hide();
-          $fig_tog.html('Show Figures')
-            .toggleClass('hide');
-        },function () {
-          $wrap.show();
-          $figure_thmbs.find('div.buttons').show();
-          $figure_thmbs.find('div.controls').show();
-          $fig_tog.html('Hide Figures')
-            .toggleClass('hide');
-        }
-      ).insertAfter($figure_thmbs)
-        .wrap('<div id="fig-toggle" class="cf" />');
     } else {
       $figure_thmbs.addClass('collapse');
     }
