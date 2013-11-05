5e763702e7f7885e3f333a2d624e17658963e664
Juan P <jperalta@plos.org>
2013-11-04 17:01:30 -0800

    dev-amec-1958 tuning sql desription

--- base/src/main/resources/org/ambraproject/service/migration/license_backfill_tentative.sql
+++ base/src/main/resources/org/ambraproject/service/migration/license_backfill_tentative.sql
@@ -1,43 +1,55 @@
-#cache cottage lab data licensing info for cited articles
-create table licenseInfo (
-  licenseID bigint not null auto_increment,
-  provenanceID bigint,
+#performance optimization duplicates some information from citedArticleLicense to minimize sql joins via hibernate
+alter table citedArticle
+    add column license varchar(30) character set utf8 collate utf8_bin,
+    add column canonicalPublicationID varchar(50) character set utf8 collate utf8_bin,
+    add index(license),
+    add constraint foreign key (canonicalPublicationID) references citedArticleLicense (canonicalPublicationID) on delete cascade;
+
+#per citation licensing information
+create table citedArticleLicense (
+  publicationIDType varchar (15) character set utf8 collate utf8_bin,
+  canonicalPublicationID varchar(50) character set utf8 collate utf8_bin,
+  status varchar(30) char set utf8 collate utf8_bin,
   maintainer varchar(30) character set utf8 collate utf8_bin,
-  family varchar(30) character set utf8 collate utf8_bin,
   title varchar(100) character set utf8 collate utf8_bin,
-  domain_data bit,
-  url varchar(100) character set utf8 collate utf8_bin,
   version varchar(30) character set utf8 collate utf8_bin,
+  domain_data bit,
   domain_content bit,
-  is_okd_compliant bit,
-  is_osi_compliant bit,
-  type varchar(30) character set utf8 collate utf8_bin,
-  jurisdiction varchar(30) character set utf8 collate utf8_bin,
   open_access bit,
   isBY bit,
   isNC bit,
   isND bit,
   isSA bit,
-  primary key (licenseID),
-  index (provenanceID),
-  index (family),
-  index (isBY),
-  index (isNC),
-  index (isSA),
-  constraint foreign key (provenanceID) references licenseProvenance(provenanceID) on delete cascade
+  provenanceCategory varchar(30) char set utf8 collate utf8_bin,
+  provenanceDescription varchar(1500) char set utf8 collate utf8_bin,
+  provenanceAgent varchar(100) char set utf8 collate utf8_bin,
+  provenanceSource varchar(100) char set utf8 collate utf8_bin,
+  provenanceDate datetime,
+  provenanceHandler varchar(30) char set utf8 collate utf8_bin,
+  HandlerVersion varchar(30) char set utf8 collate utf8_bin,
+  primary key (canonicalPublicationID),
+  index (title),
+  index (version),
+  index (status),
+  index (maintainer),
+  index(version),
+  constraint foreign key (title, version) references genericLicenseInfo (title, version)
 ) engine=innodb auto_increment=1 default charset=utf8;
 
-
-create table provenanceInfo (
-  provenanceID bigint,
-  category varchar(30) character set utf8 collate utf8_bin,
-  description  varchar(100) character set utf8 collate utf8_bin,
-  agent varchar(50) character set utf8 collate utf8_bin,
-  source  varchar(100) character set utf8 collate utf8_bin,
-  date datetime,
-  handler  varchar(30) character set utf8 collate utf8_bin,
-  handlerVersion varchar(30) character set utf8 collate utf8_bin,
-  primary key (provenanceID),
-  index (category),
-  index (handler)
+#contains common licensing information
+create table genericLicenseInfo(
+  title varchar(100) character set utf8 collate utf8_bin,
+  version varchar(30) character set utf8 collate utf8_bin,
+  family varchar(30) character set utf8 collate utf8_bin,
+  description varchar(1500) character set utf8 collate utf8_bin,
+  is_okd_compliant bit,
+  is_osi_compliant bit,
+  url varchar(100) character set utf8 collate utf8_bin,
+  domain_software bit,
+  type varchar(30) character set utf8 collate utf8_bin,
+  jurisdiction varchar(30) character set utf8 collate utf8_bin,
+  primary key (title, version),
+  index (family),
+  index (is_okd_compliant),
+  index (is_osi_compliant)
 ) engine=innodb auto_increment=1 default charset=utf8;
