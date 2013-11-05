18e5ef0c6552fdd1e70c1166971b7547b7e739f0
Juan P <jperalta@plos.org>
2013-11-05 14:10:06 -0800

    dev-amec-1958 sql migration code review

--- base/src/main/resources/org/ambraproject/service/migration/license_backfill_tentative.sql
+++ base/src/main/resources/org/ambraproject/service/migration/license_backfill_tentative.sql
@@ -1,45 +1,10 @@
 #performance optimization duplicates some information from citedArticleLicense to minimize sql joins via hibernate
 alter table citedArticle
-    add column license varchar(30) character set utf8 collate utf8_bin,
-    add column canonicalPublicationID varchar(50) character set utf8 collate utf8_bin unique,
     add column citedArticleLicenseID bigint(20),
     add constraint foreign key (citedArticleLicenseID) references citedArticleLicense (citedArticleLicenseID);
 
-#per citation licensing information
-create table citedArticleLicense (
-  citedArticleLicenseID bigint(20),
-  licenseID bigint(20),
-  canonicalPublicationID varchar(50) character set utf8 collate utf8_bin unique,
-  publicationIDType varchar (15) character set utf8 collate utf8_bin,
-  status varchar(30) char set utf8 collate utf8_bin,
-  maintainer varchar(30) character set utf8 collate utf8_bin,
-  title varchar(100) character set utf8 collate utf8_bin,
-  version varchar(30) character set utf8 collate utf8_bin,
-  domain_data bit,
-  domain_content bit,
-  open_access bit,
-  isBY bit,
-  isNC bit,
-  isND bit,
-  isSA bit,
-  provenanceCategory varchar(30) char set utf8 collate utf8_bin,
-  provenanceDescription varchar(1500) char set utf8 collate utf8_bin,
-  provenanceAgent varchar(100) char set utf8 collate utf8_bin,
-  provenanceSource varchar(100) char set utf8 collate utf8_bin,
-  provenanceDate datetime,
-  provenanceHandler varchar(30) char set utf8 collate utf8_bin,
-  HandlerVersion varchar(30) char set utf8 collate utf8_bin,
-  primary key (citedArticleLicenseID),
-  index (title),
-  index (version),
-  index (status),
-  index (maintainer),
-  index(version),
-  constraint foreign key (licenseID) references License (licenseID)
-) engine=innodb auto_increment=1 default charset=utf8;
-
 #contains common licensing information
-create table License (
+create table license (
   licenseID bigint(20),
   title varchar(100) character set utf8 collate utf8_bin,
   version varchar(30) character set utf8 collate utf8_bin,
@@ -47,11 +12,41 @@ create table License (
   description varchar(1500) character set utf8 collate utf8_bin,
   is_okd_compliant bit,
   is_osi_compliant bit,
-  url varchar(100) character set utf8 collate utf8_bin,
+  url varchar(100) character set utf8 collate utf8_bin, #ask jLin
   domain_software bit,
+  domain_data bit,
+  domain_content bit,
+  isBY bit,
+  isNC bit,
+  isND bit,
+  isSA bit,
   type varchar(30) character set utf8 collate utf8_bin,
   jurisdiction varchar(30) character set utf8 collate utf8_bin,
+  created datetime,
+  lastModified datetime,
   primary key (licenseID),
   index (family),
+  index (type),
   unique index (title, version)
+) engine=innodb auto_increment=1 default charset=utf8;
+
+#per citation licensing information
+create table citedArticleLicense (
+  citedArticleLicenseID bigint(20),
+  licenseID bigint(20),
+  status varchar(30) char set utf8 collate utf8_bin,
+  maintainer varchar(30) character set utf8 collate utf8_bin,
+  open_access bit,
+  provenanceCategory varchar(30) char set utf8 collate utf8_bin,
+  provenanceDescription varchar(1500) char set utf8 collate utf8_bin,
+  provenanceAgent varchar(100) char set utf8 collate utf8_bin,
+  provenanceSource varchar(100) char set utf8 collate utf8_bin,
+  provenanceDate datetime,
+  provenanceHandler varchar(30) char set utf8 collate utf8_bin,
+  HandlerVersion varchar(30) char set utf8 collate utf8_bin,
+  created datetime,
+  lastModified datetime,
+  primary key (citedArticleLicenseID),
+  constraint foreign key (licenseID) references license (licenseID),
+  index (status)
 ) engine=innodb auto_increment=1 default charset=utf8;
