9b46c1016020beac9a569716d2afb7d0189572e3
saghajani <saghajani@plos.org>
2014-01-27 12:08:47 -0800

    AMEC-2228 removed the extra ;

--- base/src/main/resources/org/ambraproject/service/migration/migrate_ambra_1001.sql
+++ base/src/main/resources/org/ambraproject/service/migration/migrate_ambra_1001.sql
@@ -6,6 +6,6 @@ alter table articleAsset
   modify column doi varchar(150) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL;
 
 update annotation set title = replace(title, "RE: Minor Correction: ", "RE: Publisher's Note: " );
-update annotation set title = replace(title, "Minor Correction: ", "" ) where type = 'MinorCorrection';;
+update annotation set title = replace(title, "Minor Correction: ", "" ) where type = 'MinorCorrection';
 update annotation set title = replace(title, "Minor correction: ", "" ) where type = 'MinorCorrection';
 update annotation set type = 'Comment', title = concat("Publisher's Note: ", title) where type = 'MinorCorrection';
